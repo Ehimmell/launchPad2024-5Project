72b235b72dca6270ed7ff0f9491661f1bdb7ef91
Ehimmell <himmellethan@gmail.com>
2024-09-19 15:01:58 -0400

    initial commit

---
+++ webScraper.py
@@ -0,0 +1,102 @@
+import selenium as se
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+def scrape(profName):
+    driver = se.webdriver.Chrome()
+    driver.get("https://www.ratemyprofessors.com")
+
+    def check_ad():
+        try:
+            ad = driver.find_element(By.XPATH, "//a[@class='bx-close bx-close-link bx-close-inside']")
+            ad.click()
+        except:
+            print("No ad found")
+            return None
+
+    while True:
+        try:
+            wait = WebDriverWait(driver, 2)
+            pop_up_button = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[text()='Close']")))
+
+            pop_up_button.click()
+        except TimeoutException:
+            break
+    check_ad()
+    search = driver.find_element(By.XPATH, "//input[@type='text']")
+
+    search.send_keys("Purdue University West Lafayette")
+
+    search.send_keys(Keys.RETURN)
+    check_ad()
+    schoolOrProf = driver.find_element(By.XPATH,
+                                       "//button[@class='TeacherSchoolToggleButton__StyledToggleButton-sc-15dbb0q-0 gmfVsA']")
+
+    schoolOrProf.click()
+    check_ad()
+    prof = driver.find_element(By.XPATH,
+                               "//div[@class='TeacherSchoolToggleButton__StyledTeacherSchoolDropdown-sc-15dbb0q-3 ikEasa']")
+    prof.click()
+    check_ad()
+    profNameInput = driver.find_element(By.XPATH, "//input[@placeholder='Professor name']")
+
+    profNameInput.send_keys(profName)
+
+    schoolName = driver.find_element(By.XPATH, "//input[@placeholder='Your school']")
+
+    schoolName.send_keys("Purdue University - West Lafayette")
+
+    profNameInput.send_keys(Keys.RETURN)
+    sleep(1)
+
+    clear = driver.find_element(By.XPATH, "//button[@title='Clear']")
+
+    clear.click()
+
+    profs = driver.find_elements(By.XPATH, "//a[@class='TeacherCard__StyledTeacherCard-syjs0d-0 dLJIlx']")
+
+    check_ad()
+
+    try:
+        profs[0].click()
+    except:
+        driver.quit()
+
+    check_ad()
+
+    sleepsecs = 0
+
+    while True:
+        try:
+            load_more = driver.find_element(By.XPATH, "//button[text()= 'Load More Ratings']")
+            break
+        except:
+            sleep(0.1)
+            continue
+
+    while True:
+        try:
+            load_more = driver.find_element(By.XPATH, "//button[text()= 'Load More Ratings']")
+            load_more.click()
+            sleep(2)
+        except:
+            break
+
+    check_ad()
+
+    texts = driver.find_elements(By.XPATH,
+                                 "//li//div//div//div//div[@class='Comments__StyledComments-dzzyvm-0 gRjWel']")
+
+    reviews = []
+
+    for text in texts:
+        reviews.append(text.text)
+
+    driver.quit()
+
+    return reviews
