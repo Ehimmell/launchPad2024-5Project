091748b809a01e4c206a5962d6f2584939dda650
Ehimmell <himmellethan@gmail.com>
2024-09-28 23:11:57 -0400

    add progress on the main page

--- site/website-make-angular/src/app/app.component.ts
+++ site/website-make-angular/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgOptimizedImage} from "@angular/common";
+import {SearchbarComponent} from "./searchbar/searchbar.component";
+import {QaComponent} from "./qa/qa.component";
+import {DisclaimerComponent} from "./disclaimer/disclaimer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet, NgOptimizedImage, SearchbarComponent, QaComponent, DisclaimerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
