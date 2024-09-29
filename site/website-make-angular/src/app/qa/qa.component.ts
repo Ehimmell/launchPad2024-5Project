e2e2bdde7108b4851732e88abfa982d0ec795875
Ehimmell <himmellethan@gmail.com>
2024-09-28 23:27:42 -0400

    finish basic version of question-answer component

--- site/website-make-angular/src/app/qa/qa.component.ts
+++ site/website-make-angular/src/app/qa/qa.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {QuestionAnswerComponent} from "../question-answer/question-answer.component";
 
 @Component({
   selector: 'app-qa',
   standalone: true,
-  imports: [],
+  imports: [
+    QuestionAnswerComponent
+  ],
   templateUrl: './qa.component.html',
   styleUrl: './qa.component.css'
 })
--- site/website-make-angular/src/app/question-answer/question-answer.component.ts
+++ site/website-make-angular/src/app/question-answer/question-answer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-question-answer',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './question-answer.component.css'
 })
 export class QuestionAnswerComponent {
-
+  @Input() question: string | undefined;
+  @Input() answer: string | undefined;
 }
