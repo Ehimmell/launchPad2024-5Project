172d680241a2b4525c3ecf568f05adc12e15b494
Ehimmell <himmellethan@gmail.com>
2024-09-29 21:12:21 -0400

    add emojis to font page blurbs, create professor card component

---
+++ site/website-make-angular/src/app/professor-card/professor-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './professor-card.component.html',
+  styleUrl: './professor-card.component.css'
+})
+export class ProfessorCardComponent {
+
+}
