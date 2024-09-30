3cd4f519d8377542b8bd7c4c1ee0074c77b7892d
Ehimmell <himmellethan@gmail.com>
2024-09-29 21:54:33 -0400

    idk what the last commit was

--- site/website-make-angular/src/app/app.component.ts
+++ site/website-make-angular/src/app/app.component.ts
@@ -4,11 +4,12 @@ import {NgOptimizedImage} from "@angular/common";
 import {SearchbarComponent} from "./searchbar/searchbar.component";
 import {QaComponent} from "./qa/qa.component";
 import {DisclaimerComponent} from "./disclaimer/disclaimer.component";
+import {ProfessorCardComponent} from "./professor-card/professor-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, SearchbarComponent, QaComponent, DisclaimerComponent],
+  imports: [RouterOutlet, NgOptimizedImage, SearchbarComponent, QaComponent, DisclaimerComponent, ProfessorCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
