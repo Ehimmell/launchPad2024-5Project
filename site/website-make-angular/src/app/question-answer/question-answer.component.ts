a1011da72af1ab10bf0226530d4a04686c6db84e
Ehimmell <himmellethan@gmail.com>
2024-09-29 20:55:37 -0400

    add dropdowns for qa section

--- site/website-make-angular/src/app/question-answer/question-answer.component.ts
+++ site/website-make-angular/src/app/question-answer/question-answer.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-question-answer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './question-answer.component.html',
   styleUrl: './question-answer.component.css'
 })
 export class QuestionAnswerComponent {
   @Input() question: string | undefined;
   @Input() answer: string | undefined;
+  answerVis: boolean = false;
+  dropDownCarat: string = this.answerVis ? '▲' : '▼';
+
+  toggleAnswer() {
+    this.answerVis = !this.answerVis;
+    this.dropDownCarat = this.answerVis ? '▲' : '▼';
+  }
 }
