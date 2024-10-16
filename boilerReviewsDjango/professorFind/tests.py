07204187bfc21a27328c9e10ea99a4790a8ddb2e
Ehimmell <himmellethan@gmail.com>
2024-10-15 22:03:48 -0400

    stuff, probably broken lol

--- boilerReviewsDjango/professorFind/tests.py
+++ boilerReviewsDjango/professorFind/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 import requests
 
-id = requests.get("http://127.0.0.1:8000/professorFind/professor?name=Andrew%20Freed").json()[3]
+id = requests.get("http://127.0.0.1:8000/professorFind/professor/?name=Andrew%20Freed").json()
+
+print(id)
+
+id = id[3]
 
 id = id.replace('-','')
 
-print(requests.get(f'http://127.0.0.1:8000/professorFind/classes?uuid={id}').json())
+print(requests.get(f'http://127.0.0.1:8000/professorFind/classes/?uuid={id}').json())
