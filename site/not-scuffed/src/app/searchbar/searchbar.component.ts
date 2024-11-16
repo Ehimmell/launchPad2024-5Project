60fce6a4340a8ea18ab938e9210fa29a7ca271de
Ehimmell <himmellethan@gmail.com>
2024-11-16 02:49:44 -0500

    add working results bar

--- site/not-scuffed/src/app/searchbar/searchbar.component.ts
+++ site/not-scuffed/src/app/searchbar/searchbar.component.ts
@@ -1,82 +1,87 @@
-import {AfterViewInit, Component, Inject, PLATFORM_ID} from '@angular/core';
-import {isPlatformBrowser, NgForOf, NgIf} from '@angular/common';
-import {from, Observable} from 'rxjs';
-import axios from 'axios';
-import {FormsModule} from '@angular/forms';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common'; // Import CommonModule
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
   imports: [
-    NgForOf,
-    NgIf,
-    FormsModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
   ],
   templateUrl: './searchbar.component.html',
-  styleUrl: './searchbar.component.css'
+  styleUrls: ['./searchbar.component.css']
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements AfterViewInit {
   searchInput: HTMLElement | null = null;
   private apiUrl = 'http://127.0.0.1:8000/';
   public name: string = '';
   results: string[] = [];
-  professorInfo: string[] = [];
-  classInfo: string[] = [];
+  professorInfo: any[] = [];
+  classInfo: any[] = [];
   gpas: number[] = [];
   classes: string[] = [];
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-  }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private http: HttpClient
+  ) {}
+
+  ngAfterViewInit(): void {}
 
   onEnter() {
     if (isPlatformBrowser(this.platformId)) {
-      console.log(this.name)
+      console.log('Search Query:', this.name);
       this.search().subscribe(results => {
         this.results = results;
+        console.log('Search Results:', this.results);
       });
     }
   }
 
   search(): Observable<string[]> {
-    return from(axios.get<string[]>(`${this.apiUrl}search/?name=${this.name}`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        this.handleError(error);
-        throw error;
-      }));
+    const encodedName = encodeURIComponent(this.name);
+    return this.http.get<string[]>(`${this.apiUrl}search/?name=${encodedName}`)
+      .pipe(
+        catchError(error => {
+          this.handleError(error);
+          return of([]);
+        })
+      );
   }
 
-  getProfessor(): Observable<string[]> {
-    return from(axios.get<string[]>(`${this.apiUrl}professorFind/professor/?name=${this.replaceSpaces(this.name)}`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        this.handleError(error);
-        throw error;
-      }));
+  getProfessor(): Observable<any[]> {
+    const encodedName = encodeURIComponent(this.name);
+    return this.http.get<any[]>(`${this.apiUrl}professorFind/professor/?name=${encodedName}`)
+      .pipe(
+        catchError(error => {
+          this.handleError(error);
+          return of([]);
+        })
+      );
+  }
+
+  getClasses(): Observable<any[]> {
+    const uuid = this.professorInfo[3] ? this.removeDashes(this.professorInfo[3]) : '';
+    return this.http.get<any[]>(`${this.apiUrl}professorFind/classes/?uuid=${uuid}`)
+      .pipe(
+        catchError(error => {
+          this.handleError(error);
+          return of([]);
+        })
+      );
   }
 
   removeDashes(str: string): string {
-    const regex = new RegExp('-', 'g');
-    return str.replace(regex, '');
+    return str.replace(/-/g, '');
   }
 
   replaceSpaces(str: string): string {
     return str.replace(/ /g, '%20');
-  }
-
-  getClasses(): Observable<string[]> {
-    return from(axios.get<string[]>(`${this.apiUrl}professorFind/classes/?uuid=${this.removeDashes(this.professorInfo[3])}`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        this.handleError(error);
-        throw error;
-      }));
   }
 
   onNameClick(name: string) {
@@ -85,16 +90,16 @@ export class SearchbarComponent {
       this.professorInfo = professorInfo;
       this.getClasses().subscribe(classInfo => {
         this.classInfo = classInfo;
-        for (let i = 0; i < this.classInfo.length; i++) {
-          this.gpas.push(parseFloat(this.classInfo[i][2]));
-          this.classes.push(this.classInfo[i][1]);
-        }
+        this.gpas = this.classInfo.map(info => parseFloat(info[2]));
+        this.classes = this.classInfo.map(info => info[1]);
+        console.log('GPA Array:', this.gpas);
+        console.log('Classes Array:', this.classes);
       });
     });
   }
 
   handleError(error: any) {
-    if (error.code === 'ERR_NETWORK') {
+    if (error.status === 0) {
       console.error('Network error: Please check your internet connection or backend server.');
     } else {
       console.error('An error occurred:', error.message);
